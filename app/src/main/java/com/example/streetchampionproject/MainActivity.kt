0bc160c8f2862d2eadbdaa91e0f89a340fb7c204
artornado <16artem1999@mail.ru>
2020-03-31 18:53:42 +0300

    add authorization

--- app/src/main/java/com/example/streetchampionproject/MainActivity.kt
+++ app/src/main/java/com/example/streetchampionproject/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.streetchampionproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.datepicker.MaterialDatePicker
+import kotlinx.android.synthetic.main.registration.*
+import kotlinx.android.synthetic.main.sign_in.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.sign_in)
+   /*     val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        btn_logIn.setOnClickListener {
+            *//*val builder : MaterialDatePicker.Builder<*> = MaterialDatePicker.Builder.datePicker()
+            val picker : MaterialDatePicker<*> = builder.build()
+            picker.show(supportFragmentManager, picker.toString())*//*
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener
+            {view, mYear, mMonth, mDayOfMonth -> tf_login.hint = ""+mYear+"/"+mMonth+"/"+mDayOfMonth }, year, month, day)
+            dpd.show()
+        }*/
     }
 }
