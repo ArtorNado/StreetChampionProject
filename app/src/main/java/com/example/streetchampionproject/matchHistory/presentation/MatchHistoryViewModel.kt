ff1d2341767878cf4e6c0610b8682cdce445b001
artornado <16artem1999@mail.ru>
2020-06-02 23:23:01 +0300

    fix code style

--- app/src/main/java/com/example/streetchampionproject/matchHistory/presentation/MatchHistoryViewModel.kt
+++ app/src/main/java/com/example/streetchampionproject/matchHistory/presentation/MatchHistoryViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.streetchampionproject.matchHistory.presentation
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.streetchampionproject.R
